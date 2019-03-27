b133305ef356efc593b7e0af4b21a8be7ee4e6d6
Jrohy <euvkzx@gmail.com>
2019-03-27 09:39:04 +0800

    加入dingtalk报警

--- dingtalk_alert.py
+++ dingtalk_alert.py
@@ -10,6 +10,16 @@ logger = logging.getLogger(__name__)
 
 dingtalk_url="https://oapi.dingtalk.com/robot/send?access_token={}"
 
+data_template="""
+{
+    "msgtype": "markdown",
+    "markdown": {
+        "title":"{0}",
+        "text":"{1}"
+     }
+}
+"""
+
 # Initial Flask app
 app = Flask(__name__)
 
@@ -22,7 +32,8 @@ def tg_handler():
         from msg_handler import alert_msg_handler
         for alert_json in content['alerts']:
             transform_msg = alert_msg_handler(alert_json)
-            requests.post(dingtalk_url.format(access_token), data=transform_msg)
+            post_data = data_template.format("test", transform_msg)
+            requests.post(dingtalk_url.format(access_token), data=post_data)
     except:
         logger.error("parse error ==> {}".format(content))
     return 'ok'
