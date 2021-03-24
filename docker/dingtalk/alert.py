c7399e8686afb2bd957d1327ad2a42658a38ba38
Jrohy <euvkzx@gmail.com>
2021-03-24 10:44:04 +0800

    优化新版dingtalk推送

--- docker/dingtalk/alert.py
+++ docker/dingtalk/alert.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import os
+import time
+import hmac
+import hashlib
+import base64
+import urllib.parse
 import logging
 import requests
 from flask import Flask, request
@@ -8,16 +14,29 @@ from flask import Flask, request
 logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(levelname)s] %(message)s')
 logger = logging.getLogger(__name__)
 
-access_token="accessToken"
+access_token=os.getenv("access_token")
+secret=os.getenv("secret")
+
 dingtalk_url=f"https://oapi.dingtalk.com/robot/send?access_token={access_token}"
 
 # Initial Flask app
 app = Flask(__name__)
 
+def generate_url():
+    global dingtalk_url
+    timestamp = str(round(time.time() * 1000))
+    secret_enc = secret.encode('utf-8')
+    string_to_sign = '{}\n{}'.format(timestamp, secret)
+    string_to_sign_enc = string_to_sign.encode('utf-8')
+    hmac_code = hmac.new(secret_enc, string_to_sign_enc, digestmod=hashlib.sha256).digest()
+    sign = urllib.parse.quote_plus(base64.b64encode(hmac_code))
+    dingtalk_url=f"{dingtalk_url}&timestamp={timestamp}&sign={sign}"
+
 @app.route('/alert', methods=['POST'])
 def alert():
     content = request.get_json()
     logger.info(f"alertmanager json == > {content}")
+    generate_url()
     try:
         from msg import alert_msg_handler
         for alert_json in content['alerts']:
@@ -31,4 +50,5 @@ def alert():
 
 if __name__ == "__main__":
     # Running server
-    app.run(host='0.0.0.0', port=9166)
+    from waitress import serve
+    serve(app, host='0.0.0.0', port=9166)
--- docker/dingtalk/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sed -i s/accessToken/"$accessToken"/ alert.py
-
-gunicorn -w 4 -b 0.0.0.0:9166 alert:app
